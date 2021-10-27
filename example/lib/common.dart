bba5d7b44c780761209ddb00025c02c73228c7db
jawa0919 <jawa0919@163.com>
2021-10-27 10:05:31 +0800

    add Common

---
+++ example/lib/common.dart
@@ -0,0 +1,14 @@
+/*
+ * @FilePath     : /hik_player/example/lib/common.dart
+ * @Date         : 2021-08-17 18:29:40
+ * @Author       : jawa0919 <jawa0919@163.com>
+ * @Description  : Common
+ */
+
+class Common {
+  static String host = "";
+  static String appKey = "";
+  static String appSecret = "";
+
+  static String cameraIndexCode = "";
+}
